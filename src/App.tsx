588650a5e203b4b7343faaf250b866d68ae8ddcb
spencer-rafada <spencer.byui@gmail.com>
2024-01-04 00:19:27 -0800

    added background color

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import TrackerCarousel from "@/components/TrackerCarousel"
 
 export default function App() {
   return (
-    <main className="w-screen h-screen">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">Spencer's 2024 Tracker</h1>
+    <main className="w-screen h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="p-4">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">Spencer's 2024 Tracker</h1>
+      </div>
       <div className="p-4 w-full h-5/6 flex justify-center align-center">
         <TrackerCarousel />
       </div>
